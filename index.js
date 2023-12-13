f42ad2f8fd462ff3b359e6dd0da7e3349dcb5183
Vash2626 <147669380+Vash2626@users.noreply.github.com>
2023-12-13 02:17:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
-function predict() {
-    var input = document.getElementById('inputImage').files[0];
-    var formData = new FormData();
-    formData.append('file', input);
-    fetch('https://vash2626.github.io/Footwear-Classification/', {
-          method: 'POST',
-          body: formData
-          })
-      .then(response => response.text())
-      .then(data => {
-            document.getElementById('predictionResult').innerText = data;
-            })
-      .catch(error => {
-        console.error('Error:', error);
-            });
-        }
+document.getElementById('predictButton').addEventListener('click', predict);
+
+async function predict() {
+    const input = document.getElementById('inputImage').files[0];
+
+    if (!input) {
+        alert('Please select an image.');
+        return;
+    }
+
+    const session = new onnx.InferenceSession();
+    await session.loadModel('mymodel.onnx'); 
+
+    const tensor = new onnx.Tensor(new Float32Array(3 * 224 * 224), 'float32', [1, 3, 224, 224]);
+    const imageData = getImageData(input);
+    tensor.data.set(imageData);
+
+    const inputMap = {};
+    inputMap[session.inputNames[0]] = tensor;
+    const outputMap = await session.run(inputMap);
+    
+    const predictions = outputMap.values().next().value.data;
+
+    document.getElementById('predictionResult').innerText = `Predictions: ${predictions}`;
+}
+
+function getImageData(file) {
+    return new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.onload = function (event) {
+            const image = new Image();
+            image.src = event.target.result;
+            image.onload = function () {
+                const canvas = document.createElement('canvas');
+                canvas.width = 224;
+                canvas.height = 224;
+                const ctx = canvas.getContext('2d');
+                ctx.drawImage(image, 0, 0, 224, 224);
+                const imageData = ctx.getImageData(0, 0, 224, 224).data;
+                resolve(Array.from(imageData));
+            };
+        };
+        reader.readAsDataURL(file);
+    });
+}
