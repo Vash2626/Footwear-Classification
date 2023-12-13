270189363947ea6b211413b0b6e14dbda42e94c2
Vash2626 <147669380+Vash2626@users.noreply.github.com>
2023-12-13 13:53:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-import * as tf from '@tensorflow/tfjs';
+// Function to load the model and perform prediction
+async function predict() {
+    // Create a session
+    const session = await ort.InferenceSession.create('path/to/your/model.onnx');
 
-async function loadModel() {
- const modelUrl = 'Users/VASHIST/OneDrive/Desktop/MyProject/mymodel.onnx';
- const model = await tf.loadGraphModel(modelUrl);
- return model;
+    // Create a tensor from your input data
+    const inputTensor = new ort.Tensor('float32', new Float32Array([1, 2, 3, 4]), [2, 2]);
+
+    // Feed the input tensor to the model
+    const inputFeed = { 'input_name': inputTensor };
+
+    // Run the model with the provided input
+    const outputMap = await session.run(inputFeed);
+
+    // Read the output tensor from the output map
+    const outputTensor = outputMap.get('output_name');
+
+    // Convert the output tensor to a JavaScript array
+    const outputArray = await outputTensor.data();
+
+    // Print the prediction result
+    console.log('Prediction:', outputArray);
 }
 
-async function runModel(model, inputTensor) {
- const outputTensor = await model.execute(inputTensor);
- return outputTensor;
-}
-
-(async function main() {
- const model = await loadModel();
- const inputTensor = tf.tensor(Float32Array.from([/* your input data */]), [1, 3, 60, 60]); // or your model's expected input size
- const outputTensor = await runModel(model, inputTensor);
- console.log('Model output:', outputTensor.dataSync());
-})();
+// Call the predict function
+predict();
